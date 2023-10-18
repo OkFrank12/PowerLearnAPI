3e358161ac4a5ae6748f63c48567986e3b88a727
umeh emmanuel <eumeh3882@gmail.com>
2023-10-18 06:18:19 -0700

    emma

--- utils/interface.ts
+++ utils/interface.ts
@@ -1,13 +1,51 @@
-import mongoose from "mongoose"
-
+import mongoose from "mongoose";
 
 export interface iUser {
-    email: string
-    password: string
-    verified: boolean
-    token: string
-    profile:Array<string>
-    history:Array<string>
+  email: string;
+  password: string;
+  verified: boolean;
+  token: string;
+  profile: Array<string>;
+  history: Array<string>;
 }
 
-export interface iUserData extends iUser, mongoose.Document{}
+export interface iProfile {
+  avatar: string;
+  avatarID: string;
+  name: string;
+  phoneNumber: string;
+  address: string;
+  aboutUs: string;
+  beg:Array<string>
+  user: {};
+}
+
+export interface iBeg {
+    title: string;
+    motivation: string;
+    description: string;
+    amountNeeded: number;
+    amountRaised: number;
+
+    image: string;
+    imageID: string;
+
+    like:Array<string>;
+    checkOut:Array<string>
+
+    profile:{}
+}
+
+export interface iCheckOut {
+    email:string;
+    amount:number;
+
+    beg:{}
+}
+
+export interface iUserData extends iUser, mongoose.Document {}
+export interface iProfileData extends iProfile, mongoose.Document {}
+
+export interface iBegData extends iBeg, mongoose.Document {}
+
+export interface iCheckOutData extends iCheckOut, mongoose.Document {}
